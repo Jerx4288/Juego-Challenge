a920595db915cd431cd5e01556e7f46160233682
Jherson Medrano <mbjherson@gmail.com>
2025-01-22 00:03:58 -0500

    Finalizacion del primer desafio

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver 
+// el problema.
+
+let amigos = [];
+let texto1 = document.querySelector("h2")
+
+function agregarAmigo()
+{
+    let amigoa = document.getElementById("amigo").value;
+    console.log(amigoa)
+    if (amigoa != null && amigoa != '')
+    {
+        console.log("paso")
+        amigos.push(amigoa);
+        document.querySelector("#amigo").value = '';
+        console.log(amigos);
+    }else 
+    {
+        amigoa.value = '';
+        alert("Por favor, inserte un nombre")
+    }
+    
+}
+
+function sortearAmigo()
+{
+    
+}
